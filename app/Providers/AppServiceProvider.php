57a0694cfa1afa048362f7bbce4b9496989b3497
coltrida <coltrida@gmail.com>
2022-07-28 22:48:32 +0200

    allungata la key di email

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Http\Resources\ActivityResource;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         ActivityResource::withoutWrapping();
+        Schema::defaultStringLength(191);
     }
 }
